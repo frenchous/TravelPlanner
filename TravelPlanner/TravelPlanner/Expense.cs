b53ee4816274464192275423b8b9215983103d71
frenchous <komnata3fff@gmail.com>
2026-03-18 17:42:16 +0300

    2. Добавлен новый класс: Expense.cs

---
+++ TravelPlanner/TravelPlanner/Expense.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPlanner
+{
+    public class Expense
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public Expense(string description, decimal amount)
+        {
+            Description = description;
+            Amount = amount;
+            Date = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{Description} - {Amount:C} ({Date:dd.MM.yyyy})";
+        }
+    }
+}
